b7347da2855930900c62ed2ad34ea5ade53f1cb8
Danny Ruiz <ynnadrules@gmail.com>
2019-08-07 11:45:52 -0500

    Update build tools version

--- src/main/groovy/com/readdle/android/swift/gradle/SwiftAndroidPluginExtension.groovy
+++ src/main/groovy/com/readdle/android/swift/gradle/SwiftAndroidPluginExtension.groovy
@@ -87,7 +87,7 @@ class SwiftAndroidPluginExtension {
     boolean cleanEnabled = true
     boolean usePackageClean = true
     boolean useKapt = false
-    String toolsVersion = "swift5"
+    String toolsVersion = "1.9.1-swift5"
 
     SwiftAndroidPluginExtension(Project project) {
         this.project = project
